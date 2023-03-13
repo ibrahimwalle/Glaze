1e7223b830625bd52fb7c3b5865291458d5c64c6
Ibrahim Wali <dinowali9@gmail.com>
2023-03-13 21:46:50 +0000

    Work component

--- src/App.jsx
+++ src/App.jsx
@@ -112,7 +112,7 @@ function App() {
         </div>
         <Routes>
           <Route path='/home' element={<HomeComponent workCards={workCards} slideShow={slideShow} setActiveLink={setActiveLink}/>}/>
-          <Route path='/work' element={<WorkComponent />}/>
+          <Route path='/work' element={<WorkComponent workCards={workCards}/>}/>
           <Route path='/services' element={null}/>
           <Route path='/about' element={null}/>
         </Routes>
@@ -284,10 +284,27 @@ function ConnectAnimation(props) {
 )}
 
 
-function WorkComponent(props) {
+function WorkComponent({workCards}) {
+  const archived = [
+    {'year':`2019`, 'title': `Abernathy Group	Automated - Innovative E-Tailers`},
+    {'year':`2018`, 'title': `Stokes-Rath - Distributed Discrete Solution`},
+    {'year':`2014`, 'title': `Trantow-Osinski - Extend Enterprise E-Services`}];
   return(
-    <div>
-      <h1>Hello</h1>
+    <div className='workComponent'>
+      <br /><br /><br />
+      <section>
+          <h2>Featured work</h2>
+          <FeaturedWork workCards={workCards}/>
+      </section>
+      <section>
+        <h2>Archived</h2>
+        <div className="archivedWorks">
+          {archived.map((work,i)=><div key={i} className='archived'>
+            <p className='year'>{work.year}</p>
+            <p className='archivedTitle'>{work.title}</p>
+          </div>)}
+        </div>
+      </section>
     </div>
 )}
 
