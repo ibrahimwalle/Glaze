1d768cdaeeee0ed52f9341fbab8cc48c1f20ed61
Ibrahim Wali <dinowali9@gmail.com>
2023-04-05 12:37:05 +0100

    vite config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,18 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    vue({
+      // This is needed, or else Vite will try to find image paths (which it wont be able to find because this will be called on the web, not directly)
+      // For example <img src="/images/logo.png"> will not work without the code below
+      template: {
+          transformAssetUrls: {
+              includeAbsolute: false,
+          },
+      },
+    }),
+  ],
   test:{
     globals: true,
     environment: 'jsdom',
